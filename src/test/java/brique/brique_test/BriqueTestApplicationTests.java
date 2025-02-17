e8c2b158ebb8740ff4db417b3e07136b97e09bb2
jjinjukim <developer.do.de.gee@gmail.com>
2025-02-17 17:07:20 +0900

    Initial commit

---
+++ src/test/java/brique/brique_test/BriqueTestApplicationTests.java
@@ -0,0 +1,13 @@
+package brique.brique_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BriqueTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
